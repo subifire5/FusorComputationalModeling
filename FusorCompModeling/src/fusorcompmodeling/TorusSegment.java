0c9a13d2922ed5cfe9e4c83319e7ce0d543a50aa
Gavin Uberti <gavin.uberti@gmail.com>
2017-01-02 22:43:21 -0800

    Revert "Redo algorithms to calculate starting point coming off of a torus"
    This reverts commit c776641dec9e926d13c53c172923541b169f81a5.

--- FusorCompModeling/src/fusorcompmodeling/TorusSegment.java
+++ FusorCompModeling/src/fusorcompmodeling/TorusSegment.java
@@ -15,7 +15,6 @@ public class TorusSegment extends GridComponent {
     double phi2;
     double phi3;
     double radius2;
-    Vector startPos;
     
     public TorusSegment (Vector pos, double radius, double phi2, double phi3, double radius2, int charge) {
         this.pos = pos;
@@ -25,16 +24,6 @@ public class TorusSegment extends GridComponent {
         this.radius2 = radius2; // Width of the torus
         this.charge = charge;
         this.type = ComponentType.TorusSegment;
-    }
-    
-    // When constructing torus segments as part of a wire, saving some of the
-    // calculated data when the torus segment is placed in the vector
-    // endPos is helpful in calculating the position of the next cylinder
-    
-    public TorusSegment (Vector pos, double radius, double phi2, double phi3, double radius2, int charge, Vector startPos) {
-        this(pos, radius, phi2, phi3, radius2, charge);
-        this.startPos = startPos;
-        
     }
     
     @Override
