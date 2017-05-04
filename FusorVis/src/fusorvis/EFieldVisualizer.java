f413623acecfef711417180ac4bb94cf202339df
Gavin Uberti <gavin.uberti@gmail.com>
2017-05-04 16:48:29 -0700

    Create global reference

--- FusorVis/src/fusorvis/EFieldVisualizer.java
+++ FusorVis/src/fusorvis/EFieldVisualizer.java
@@ -29,7 +29,16 @@ public class EFieldVisualizer {
     int blockSideLength;
     PixelWriter w;
     Point[] points;
+        
+    final int[] xRange = {-20, 20};
+    final int[] yRange = {-20, 20};
+    final int[] zRange = {-5, 5};
     
+    final int rangeStep = 1;
+    
+    double[] refVals;
+    
+    int l = ((xRange[1] - xRange[0])*(yRange[1] - yRange[0])*(zRange[1] - zRange[0])) / ((int) Math.pow(rangeStep, 3));
     
     public EFieldVisualizer(double sliceWidth, double sliceHeight, double cF, int blockSideLength, PixelWriter w, Point[] points) {
         System.out.println(EField.kQ);
@@ -45,6 +54,11 @@ public class EFieldVisualizer {
         this.points = points;
         this.blockSideLength = blockSideLength;
         this.w = w;
+        
+        refVals = new double[((xRange[1] - xRange[0])*(yRange[1] - yRange[0])*(zRange[1] - zRange[0])) / ((int) Math.pow(rangeStep, 3))];
+        
+        getEFieldRange();
+        int foo = 1;
     }
     
     
@@ -67,6 +81,24 @@ public class EFieldVisualizer {
         }
         
         return fieldGrid;
+    }
+    
+    public void getEFieldRange() {
+        int jMult = (zRange[1] - zRange[0]) / rangeStep;
+        int iMult = ((yRange[1] - yRange[0]) / rangeStep) * jMult;
+        for (int i = xRange[0]; i < xRange[1]; i += rangeStep) {
+            
+            for (int j = yRange[0]; j < yRange[1]; j += rangeStep) {
+                for (int k = zRange[0]; k < zRange[1]; k += rangeStep) {
+                    int ind = (k - zRange[0]) / rangeStep;
+                    ind += jMult * ((j - yRange[0]) / rangeStep);
+                    ind += iMult * ((i - xRange[0]) / rangeStep);
+                    
+                    refVals[ind] = PointDistributer.electricPotential(points, new Point(i, j, k));
+                }
+            }
+        }
+        Arrays.sort(refVals);
     }
     
     public void renderRGBCodedVectors(Rotate[] eFieldTransforms, Box eFieldSlice) {
@@ -161,7 +193,6 @@ public class EFieldVisualizer {
     
     public void renderElectricPotential(Rotate[] eFieldTransforms, Box eFieldSlice) {
         double[][] fieldGrid = new double[aW][aH];
-        double[] sorted = new double[aW * aH];
 
         for (int i = 0; i < aW; i++) {
             for (int k = 0; k < aH; k++) {
@@ -169,15 +200,12 @@ public class EFieldVisualizer {
                 p = translateEFieldPixel(p, eFieldTransforms, eFieldSlice);
                 
                 fieldGrid[i][k] = PointDistributer.electricPotential(points, p);
-                sorted[k * aW + i] = fieldGrid[i][k];
             }
         }
-
-        Arrays.sort(sorted);
                 
         for (int i = 0; i < aW; i++) {
             for (int k = 0; k < aH; k++) {
-                double gV = toColor(indexOf(sorted, fieldGrid[i][k]), sorted.length);
+                double gV = toColor(closestIndex(refVals, fieldGrid[i][k]), refVals.length);
                 
                 Color c = new Color(gV, 0.0, 1.0 - gV, 1.0);
                 
@@ -198,9 +226,22 @@ public class EFieldVisualizer {
         return (double) index / (double) (length - 1);
     }
     
-    private void reverse(double[][] arrs) {
+    private void reverse2DArr (double[][] arrs) {
         for (double[] arr : arrs) {
-            //ArrayUtils.reverse(arr);
+            revSingleArr(arr);
+        }
+        for (int i = 0; i < (int) arrs.length/2; i++) {
+            double[] t = arrs[i];
+            arrs[i] = arrs[(arrs.length - 1) - i];
+            arrs[(arrs.length - 1) - i] = t;
+        }
+    }
+    
+    private void revSingleArr (double[] d) {
+        for (int i = 0; i < (int) d.length/2; i++) {
+            double t = d[i];
+            d[i] = d[(d.length - 1) - i];
+            d[(d.length - 1) - i] = t;
         }
     }
     
@@ -208,6 +249,7 @@ public class EFieldVisualizer {
         // Simple binary search
         int low = 0;
         int high = arr.length - 1;
+        
         while (low <= high) {
             int mid = low + (high - low) / 2;
             if (item < arr[mid]) {
@@ -221,6 +263,28 @@ public class EFieldVisualizer {
         return -1; // Will never get here
     }
     
+    private int closestIndex(double[] arr, double item) {
+        int low = 0;
+        int high = arr.length - 1;
+        
+        double lastVal;
+        int mid = -1;
+
+        while (low <= high) {
+        
+            mid = (low + high) / 2;
+            lastVal = arr[mid];
+            
+            if (item < lastVal) {
+                high = mid - 1;
+            } else if (item > lastVal) {
+                low = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return mid;
+    }
     public Point translateEFieldPixel(Point p, Rotate[] eFieldTransforms, Box eFieldSlice) {
         Point r = new Point(p.x, p.y, p.z);
         double c = 1;
