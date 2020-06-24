ef51b791145dca03f481094a6950a4c296355639
gunnarmein-ucbischool <gunnarmein@berkeley.edu>
2020-06-24 09:40:09 -0700

    Cleanup

--- JavaNeutrons/src/main/java/org/eastsideprep/javaneutrons/core/Util.java
+++ JavaNeutrons/src/main/java/org/eastsideprep/javaneutrons/core/Util.java
@@ -47,88 +47,7 @@ public class Util {
             return new Vector3D(random.nextGaussian() * sd, random.nextGaussian() * sd, random.nextGaussian() * sd);
         }
 
-        //
-        // rayTriangleIntersect
-        //
-        // static helper function
-        // ported from: 
-        // https://www.scratchapixel.com/lessons/3d-basic-rendering/ray-tracing-rendering-a-triangle/ray-triangle-intersection-geometric-solution
-        //
-// public static double rayTriangleIntersect(
-//                Vector3D orig, Vector3D dir,
-//                Vector3D v0, Vector3D v1, Vector3D v2) {
-        public static double rayTriangleIntersectOld(
-                Vector3D orig, Vector3D dir,
-                double v0x, double v0y, double v0z,
-                double v1x, double v1y, double v1z,
-                double v2x, double v2y, double v2z) {
-
-            final double kEpsilon = 1E-8; // constant for "close enough to 0"
-
-            Vector3D v0 = new Vector3D(v0x, v0y, v0z);
-            Vector3D v1 = new Vector3D(v1x, v1y, v1z);
-            Vector3D v2 = new Vector3D(v2x, v2y, v2z);
-
-//            System.out.println(v0);
-//            System.out.println(v1);
-//            System.out.println(v2);
-            // compute plane's normal
-            Vector3D v0v1 = v1.subtract(v0);
-            Vector3D v0v2 = v2.subtract(v0);
-            // no need to normalize
-            Vector3D N = v0v1.crossProduct(v0v2); // N 
-            double area2 = N.getNorm();
-
-            // Step 1: finding P
-            // check if ray and plane are parallel ?
-            double nDotDir = N.dotProduct(dir);
-
-            // almost 0?
-            if (nDotDir > -kEpsilon) {
-                return -1; // wrong direction or parallel 
-            }
-
-            // compute d parameter using equation 2
-            double d = N.dotProduct(v0);
-
-            // compute t (equation 3)
-            double t = (d - N.dotProduct(orig)) / nDotDir;
-            // check if the triangle is in behind the ray
-            if (t < 0) {
-                return -1; // the triangle is behind 
-            }
-            // compute the intersection point using equation 1
-            Vector3D P = orig.add(dir.scalarMultiply(t));
-
-            // Step 2: inside-outside test
-            Vector3D C; // vector perpendicular to triangle's plane 
-
-            // edge 0
-            Vector3D edge0 = v1.subtract(v0);
-            Vector3D vp0 = P.subtract(v0);
-            C = edge0.crossProduct(vp0);
-            if (N.dotProduct(C) < 0) {
-                return -1; // P is on the right side 
-            }
-
-            // edge 1
-            Vector3D edge1 = v2.subtract(v1);
-            Vector3D vp1 = P.subtract(v1);
-            C = edge1.crossProduct(vp1);
-            if (N.dotProduct(C) < 0) {
-                return -1; // P is on the right side 
-            }
-            // edge 2
-            Vector3D edge2 = v0.subtract(v2);
-            Vector3D vp2 = P.subtract(v2);
-            C = edge2.crossProduct(vp2);
-            if (N.dotProduct(C) < 0) {
-                return -1; // P is on the right side; 
-            }
-
-            return t; // this ray hits the triangle, return where on the ray
-        }
-
+ 
         //
         // rayTriangleIntersectNew
         //
@@ -136,137 +55,48 @@ public class Util {
         // adapted from: 
         // https://en.wikipedia.org/wiki/M%C3%B6ller%E2%80%93Trumbore_intersection_algorithm#Java_Implementation
         //
-        public static double rayTriangleIntersectOld2(
-                Vector3D orig, Vector3D dir,
-                double v0x, double v0y, double v0z,
-                double v1x, double v1y, double v1z,
-                double v2x, double v2y, double v2z) {
-            final double kEpsilon = 1E-12; // constant for "close enough to 0"
-            double a, f, u, v;
-
-            Vector3D v0 = new Vector3D(v0x, v0y, v0z);
-            Vector3D v1 = new Vector3D(v1x, v1y, v1z);
-            Vector3D v2 = new Vector3D(v2x, v2y, v2z);
-
-            Vector3D edge1 = v1.subtract(v0);
-            System.out.println("Edge1" + edge1);
-
-            Vector3D edge2 = v2.subtract(v0);
-            System.out.println("Edge2" + edge2);
-            Vector3D h = dir.crossProduct(edge2);
-            System.out.println("h" + h);
-            a = edge1.dotProduct(h);
-            if (a > -kEpsilon && a < kEpsilon) {
-                return -1;    // This ray is parallel to this triangle.
-            }
-
-            Vector3D n = edge1.crossProduct(edge2);
-            System.out.println("n" + n);
-            double nDotDir = n.dotProduct(dir);
-
-            // addition: need to check her whether the ray is goin in the wrong direction
-            if (nDotDir > -kEpsilon) {
-                return -1; // wrong direction or parallel 
-            }
-
-            f = 1.0 / a;
-            System.out.println("f " + f);
-
-            Vector3D s = orig.subtract(v0);
-            System.out.println("s" + s);
-            u = f * (s.dotProduct(h));
-            System.out.println("u " + u);
-            if (u < 0.0 || u > 1.0) {
-                return -1;
-            }
-            Vector3D q = s.crossProduct(edge1);
-            System.out.println("q" + q);
-            v = f * dir.dotProduct(q);
-            System.out.println("v " + v);
-
-            if (v < 0.0 || u + v > 1.0) {
-                return -1;
-            }
-            // At this stage we can compute t to find out where the intersection point is on the line.
-
-            double t = f * edge2.dotProduct(q);
-            System.out.println("t " + t);
-
-            if (t > kEpsilon) {
-                return t;
-            } else {
-                return -1;
-            }
-        }
-
         public static double rayTriangleIntersect(
                 double ox, double oy, double oz,
                 double dx, double dy, double dz,
                 double v0x, double v0y, double v0z,
                 double v1x, double v1y, double v1z,
                 double v2x, double v2y, double v2z) {
-            //System.out.println("");
-
-            //rayTriangleIntersectNew(orig, dir, v0x, v0y, v0z, v1x, v1y, v1z, v2x, v2y, v2z);
             final double kEpsilon = 1E-12; // constant for "close enough to 0"
             double a, f, u, v;
 
-//            Vector3D v0 = new Vector3D(v0x, v0y, v0z);
-//            Vector3D v1 = new Vector3D(v1x, v1y, v1z);
-//            Vector3D v2 = new Vector3D(v2x, v2y, v2z);
             double edge1x = v1x - v0x;
             double edge1y = v1y - v0y;
             double edge1z = v1z - v0z;
 
-            //Vector3D edge1 = v1.subtract(v0);
-            //System.out.println("Edge1" + edge1);
             double edge2x = v2x - v0x;
             double edge2y = v2y - v0y;
             double edge2z = v2z - v0z;
 
-            //Vector3D edge2 = v2.subtract(v0);
-            //System.out.println("Edge2" + edge2);
             double hx = dy * edge2z - dz * edge2y;
             double hy = dz * edge2x - dx * edge2z;
             double hz = dx * edge2y - dy * edge2x;
 
-            //Vector3D h = dir.crossProduct(edge2);
-            //System.out.println("h" + h);
             a = edge1x * hx + edge1y * hy + edge1z * hz;
 
-            //a = edge1.dotProduct(h);
             if (a > -kEpsilon && a < kEpsilon) {
                 return -1;    // This ray is parallel to this triangle.
             }
 
-            //
             double nx = edge1y * edge2z - edge1z * edge2y;
             double ny = edge1z * edge2x - edge1x * edge2z;
             double nz = edge1x * edge2y - edge1y * edge2x;
 
-            //Vector3D n = edge1.crossProduct(edge2);
-            //System.out.println("n" + n);
             double nDotDir = nx * dx + ny * dy + nz * dz;
-
-            //nDotDir = n.dotProduct(dir);
-            // addition: need to check her whether the ray is goin in the wrong direction
             if (nDotDir > -kEpsilon) {
                 return -1; // wrong direction or parallel 
             }
 
             f = 1.0 / a;
-            //System.out.println("f " + f);
-
             double sx = ox - v0x;
             double sy = oy - v0y;
             double sz = oz - v0z;
 
-            //Vector3D s = orig.subtract(v0);
-            //System.out.println("s" + s);
             u = f * (sx * hx + sy * hy + sz * hz);
-
-            //u = f * s.dotProduct(h);
-            //System.out.println("u " + u);
             if (u < 0.0 || u > 1.0) {
                 return -1;
             }
@@ -274,21 +104,14 @@ public class Util {
             double qx = sy * edge1z - sz * edge1y;
             double qy = sz * edge1x - sx * edge1z;
             double qz = sx * edge1y - sy * edge1x;
-            //Vector3D q = s.crossProduct(edge1);
-            //System.out.println("q" + q);
 
             v = f * (dx * qx + dy * qy + dz * qz);
-            //v = f * dir.dotProduct(q);
-            //System.out.println("v " + v);
-
             if (v < 0.0 || u + v > 1.0) {
                 return -1;
             }
+
             // At this stage we can compute t to find out where the intersection point is on the line.
             double t = f * (edge2x * qx + edge2y * qy + edge2z * qz);
-            //double t = f * edge2.dotProduct(q);
-            //System.out.println("t " + t);
-
             if (t > kEpsilon) {
                 return t;
             } else {
